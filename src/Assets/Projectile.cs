ff9351c2c95109c8612fa047b4271b0498097e25
Jacob Rieger <jacob@talentriot.com>
2015-02-15 19:39:15 -0700

    initial project commit with small scene work

---
+++ src/Assets/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float Speed = 10;
+	public float Range = 10;
+
+	private float _distanceTraveled;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * Speed);
+		_distanceTraveled += Time.deltaTime * Speed;
+		if(_distanceTraveled >= Range)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ src/Assets/Turret.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+	public Transform Target;
+	public GameObject Projectile;
+	public float ReloadTime = 1f;
+	public float FirePauseTime = .25f;
+	public float TurnSpeed = 5f;
+	public GameObject LauchEffect;
+	public float ErrorAmount = .001f;
+	public Transform[] projectileLaunchLocations;
+	public Transform Launcher;
+
+	public float _nextFireTime;
+	public float _aimError;
+	public float _nextMoveTime;
+	public Quaternion _desiredRotation;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Target)
+		{
+			if(Time.time >= _nextMoveTime)
+			{
+				CalculateAimPosition(Target.position);
+				Launcher.rotation = Quaternion.Lerp(Launcher.rotation, _desiredRotation, Time.deltaTime * TurnSpeed);
+			}
+			Debug.Log("Time.time = " + Time.time.ToString());
+			Debug.Log("Next Fire = " + _nextFireTime.ToString());
+			if(Time.time >= _nextFireTime)
+			{
+				FireProjectile();
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider enteringObject)
+	{
+		Debug.Log("Trigger Entered");
+		if(enteringObject.gameObject.tag == "Enemy")
+		{
+			Debug.Log ("Enemy Entered range");
+			_nextFireTime = Time.time + (ReloadTime * .5f);
+			Target = enteringObject.gameObject.transform;
+		}
+	}
+
+	void OnTriggerExit(Collider exitingObject)
+	{
+		if(exitingObject.gameObject.transform == Target)
+		{
+			Target = null;
+		}
+	}
+
+	void FireProjectile()
+	{
+		Debug.Log ("Fire Projectile");
+		_nextFireTime = Time.time + ReloadTime;
+		_nextMoveTime = Time.time + FirePauseTime;
+		CalculateAimError();
+
+		foreach(var launchLocation in projectileLaunchLocations)
+		{
+			Instantiate (Projectile, launchLocation.position, launchLocation.rotation);
+			Instantiate (LauchEffect, launchLocation.position, launchLocation.rotation);
+		}
+	}
+
+	void CalculateAimPosition(Vector3 targetPosition)
+	{
+		Debug.Log ("Calculate Aim Position");
+		var aimPoint = new Vector3(targetPosition.x, targetPosition.y, targetPosition.z + _aimError);
+		_desiredRotation = Quaternion.LookRotation(aimPoint);
+	}
+
+	void CalculateAimError()
+	{
+		_aimError = Random.Range (-ErrorAmount, ErrorAmount);
+	}
+
+}
